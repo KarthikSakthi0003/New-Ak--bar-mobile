345c37ab717e22a1ef3da5c14ca6aacea00bffde
KarthikSakthi <karthiksakthi0003@gmail.com>
2022-05-13 01:38:16 -0700

    New-Ak-Bar-mobile

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+const clicker = document.getElementById("clicker");
+clicker.addEventListener("click", () => {
+  const clickerMenu = document.querySelector(".container-radio");
+
+  clickerMenu.classList.add("show1 show1-show2");
+});
