41df2a9265b4a504583157767db2b07dc42023d0
KarthikSakthi <karthiksakthi0003@gmail.com>
2022-05-15 02:23:31 -0700

    Ak-New-Bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,56 @@
-const clicker = document.getElementById("clicker");
-clicker.addEventListener("click", () => {
-  const clickerMenu = document.querySelector(".container-radio");
+function openSM() {
+  document.getElementById("mySidemenu");
+  document.getElementById("pg-content");
+}
+function closeSM() {
+  document.getElementById("mySidemenu").style.width = "0";
+  document.getElementById("pg-content").style.marginLeft = "0";
+}
 
-  clickerMenu.classList.add("show1 show1-show2");
+//
+
+//form click
+function loginM() {
+  document.getElementById("loginMenu");
+  document.getElementById("login-content");
+}
+function logoffM() {
+  document.getElementById("loginMenu").style.width = "0";
+  document.getElementById("login-content").style.marginLeft = "0";
+}
+
+/* form login */
+const container = document.querySelector(".container"),
+  pwShowHide = document.querySelectorAll(".showHidePw"),
+  pwFields = document.querySelectorAll(".password"),
+  signUp = document.querySelector(".signup-link"),
+  login = document.querySelector(".login-link");
+
+//   js code to show/hide password and change icon
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    pwFields.forEach((pwField) => {
+      if (pwField.type === "password") {
+        pwField.type = "text";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("fa-eye-slash", "fa-eye");
+        });
+      } else {
+        pwField.type = "password";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("fa-eye", "fa-eye-slash");
+        });
+      }
+    });
+  });
+});
+
+// js code to appear signup and login form
+signUp.addEventListener("click", () => {
+  container.classList.add("active");
+});
+login.addEventListener("click", () => {
+  container.classList.remove("active");
 });
